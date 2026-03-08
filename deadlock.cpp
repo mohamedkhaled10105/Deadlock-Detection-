56eac6744ded7319f061d4b7f52abcd477694353
mohamedkhaled10105 <mohamedkhaled4343@gmail.com>
2026-03-08 08:44:24 +0000

    implement deadlock detection algorithm and print results

--- deadlock.cpp
+++ deadlock.cpp
@@ -19,18 +19,23 @@ int main()
     int C[10][10];
     int R[10][10];
     int A[10];
+    bool finish[10];
 
+    // read total resources 
     for(int i = 0; i < m; i++)
         file >> E[i];
 
+    // read allocation matrix 
     for(int i = 0; i < n; i++)
         for(int j = 0; j < m; j++)
             file >> C[i][j];
 
+    // read request matrix        
     for(int i = 0; i < n; i++)
         for(int j = 0; j < m; j++)
             file >> R[i][j];
 
+    //calculate available resources 
     for(int j = 0; j < m; j++)
     {
         int sum = 0;
@@ -40,6 +45,62 @@ int main()
 
         A[j] = E[j] - sum;
     }
+
+    // mark all processes as unfinished 
+    for(int i = 0; i < n; i++)
+        finish[i] = false;
+
+    bool change = true;
+
+    // deadlock detection algorithm 
+    while(change)
+    {
+        change = false;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(!finish[i])
+            {
+                bool possible = true;
+
+                // check if process can execute 
+                for(int j = 0; j < m; j++)
+                {
+                    if(R[i][j] > A[j])
+                        possible = false;
+                }
+
+                // if process can execute, release its resources 
+                if(possible)
+                {
+                    for(int j = 0; j < m; j++)
+                        A[j] += C[i][j];
+
+                    finish[i] = true;
+                    change = true;
+                }
+            }
+        }
+    }
+
+    bool deadlock = false;
+
+    cout << "Deadlock Detection Result:" << endl;
+    cout << "Deadlocked Processes: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        if(!finish[i])
+        {
+            cout << "P" << i << " ";
+            deadlock = true;
+        }
+    }
+
+    if(!deadlock)
+        cout << "None";
+
+    cout << endl;
 
     return 0;
 }
