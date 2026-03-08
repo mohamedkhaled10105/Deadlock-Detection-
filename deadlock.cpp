baa2387400b84f9ec4bf0c0ace993a0d481507a7
mohamedkhaled10105 <mohamedkhaled4343@gmail.com>
2026-03-08 08:07:45 +0000

    add file input and read number of processes and resources

--- deadlock.cpp
+++ deadlock.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-    cout << "Deadlock Detection Program" << endl;
+    ifstream file("input.txt");
+
+    if(!file)
+    {
+        cout << "Error opening file" << endl;
+        return 0;
+    }
+
+    int n, m;
+
+    file >> n >> m;
+
+    cout << "Processes: " << n << endl;
+    cout << "Resources: " << m << endl;
+
     return 0;
-} 
+}
